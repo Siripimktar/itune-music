22d89360b1b77185fe7008737be47265920d9f7f
Sirpimktar <68525526+Siripimktar@users.noreply.github.com>
2020-11-04 16:42:46 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="10">
+      <div class="text-center">
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to iTunes MUSIC.
+        </v-card-title>
+        <v-card-text>
+          <p />
+          <p>
+            You can click for view itunes page.
+            <a
+              href="https://www.apple.com/th/itunes/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              iTunes MUSIC page </a>.
+          </p>
+          <p>IF YOU WANT TO VIEW PROFILE CLICK CONTiUNUE</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="warning" nuxt to="/inspire">
+            Continue
+            <v-icon>mdi-cursor-default-click</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+export default {
+  components: {
+    VuetifyLogo
+  }
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    />
+  </v-carousel>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: 'https://i.imgur.com/H3DJCbA.png'
+        },
+        {
+          src: 'https://i.imgur.com/xJujQsw.png'
+        },
+        {
+          src: 'https://i.imgur.com/Qn3X5D2.png'
+        },
+        {
+          src: 'https://i.imgur.com/KjEu1mx.png'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+</style>
