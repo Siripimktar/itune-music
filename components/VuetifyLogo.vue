3f395990a53a32743e830aa210a390f81812b660
Sirpimktar <68525526+Siripimktar@users.noreply.github.com>
2020-11-04 16:40:21 +0700

    Add files via upload

---
+++ components/VuetifyLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    class="iTunemusicLogo"
+    alt="iTunemusic Logo"
+    src="https://i.imgur.com/yGF2bPO.png"
+  >
+</template>
+
+<style>
+.iTunemusicLogo{
+  height: 35%;
+  width: 35%;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
